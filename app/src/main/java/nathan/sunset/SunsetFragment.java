4cd0456a1e0d344e59b30a169053ecba2ea328f2
Nathan Davis <davisna1@appstate.edu>
2016-11-16 20:59:00 -0500

    Sun has rotating border, and tapping again while the sun is rising or setting causes it to stop and reverse direction

--- app/src/main/java/nathan/sunset/SunsetFragment.java
+++ app/src/main/java/nathan/sunset/SunsetFragment.java
@@ -4,12 +4,15 @@ import android.animation.AnimatorSet;
 import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateInterpolator;
+import android.view.animation.LinearInterpolator;
 
 /**
  * Created by Owner on 11/10/2016.
@@ -20,14 +23,13 @@ public class SunsetFragment extends Fragment {
     private View mSceneView;
     private View mSunView;
     private View mSkyView;
-    private View mSeaView;
 
     private int mBlueSkyColor;
     private int mSunsetSkyColor;
     private int mNightSkyColor;
-    private int mBlueSeaColor;
-    private int mSunsetSeaColor;
-    private int mNightSeaColor;
+
+    private AnimatorSet mSunsetAnimatorSet = new AnimatorSet();
+    private AnimatorSet mSunriseAnimatorSet = new AnimatorSet();
 
     private boolean sunUp = true;
 
@@ -42,15 +44,17 @@ public class SunsetFragment extends Fragment {
         mSceneView = view;
         mSunView = view.findViewById(R.id.sun);
         mSkyView = view.findViewById(R.id.sky);
-        mSeaView = view.findViewById(R.id.sea);
 
         Resources resources = getResources();
         mBlueSkyColor = resources.getColor(R.color.blue_sky);
         mSunsetSkyColor = resources.getColor(R.color.sunset_sky);
         mNightSkyColor = resources.getColor(R.color.night_sky);
-        mBlueSeaColor = resources.getColor(R.color.blue_sea);
-        mSunsetSeaColor = resources.getColor(R.color.sunset_sea);
-        mNightSeaColor = resources.getColor(R.color.night_sea);
+
+        ObjectAnimator sunAnimator = ObjectAnimator.ofFloat(mSunView, "rotation", 0, 90)
+                .setDuration(4000);
+        sunAnimator.setRepeatCount(ObjectAnimator.INFINITE);
+        sunAnimator.setInterpolator(new LinearInterpolator());
+        sunAnimator.start();
 
         mSceneView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,7 +72,7 @@ public class SunsetFragment extends Fragment {
     }
 
     private void startSunsetAnimation() {
-        float sunYStart = mSunView.getTop();
+        float sunYStart = mSunView.getY();
         float sunYEnd = mSkyView.getHeight();
 
         ObjectAnimator heightAnimator = ObjectAnimator.ofFloat(mSunView, "y", sunYStart, sunYEnd)
@@ -76,38 +80,29 @@ public class SunsetFragment extends Fragment {
         heightAnimator.setInterpolator(new AccelerateInterpolator());
 
         ObjectAnimator sunsetSkyAnimator = ObjectAnimator
-                .ofInt(mSkyView, "backgroundColor", mBlueSkyColor, mSunsetSkyColor)
+                .ofInt(mSkyView,
+                        "backgroundColor",
+                        ((ColorDrawable)mSkyView.getBackground()).getColor(),
+                        mSunsetSkyColor)
                 .setDuration(3000);
         sunsetSkyAnimator.setEvaluator(new ArgbEvaluator());
-
-        ObjectAnimator sunsetSeaAnimator = ObjectAnimator
-                .ofInt(mSeaView, "backgroundColor", mBlueSeaColor, mSunsetSeaColor)
-                .setDuration(3000);
-        sunsetSeaAnimator.setEvaluator(new ArgbEvaluator());
 
         ObjectAnimator nightSkyAnimator = ObjectAnimator
                 .ofInt(mSkyView, "backgroundColor", mSunsetSkyColor, mNightSkyColor)
                 .setDuration(1500);
         nightSkyAnimator.setEvaluator(new ArgbEvaluator());
 
-        ObjectAnimator nightSeaAnimator = ObjectAnimator
-                .ofInt(mSeaView, "backgroundColor", mSunsetSeaColor, mNightSeaColor)
-                .setDuration(1500);
-        nightSeaAnimator.setEvaluator(new ArgbEvaluator());
-
-        AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.play(heightAnimator)
+        mSunsetAnimatorSet = new AnimatorSet();
+        mSunsetAnimatorSet.play(heightAnimator)
                 .with(sunsetSkyAnimator)
                 .before(nightSkyAnimator);
-        animatorSet.play(heightAnimator)
-                .with(sunsetSeaAnimator)
-                .before(nightSeaAnimator);
-        animatorSet.start();
+        mSunriseAnimatorSet.cancel();
+        mSunsetAnimatorSet.start();
     }
 
     private void startSunriseAnimation() {
         float sunYEnd = mSunView.getTop();
-        float sunYStart = mSkyView.getHeight();
+        float sunYStart = mSunView.getY();
 
         ObjectAnimator heightAnimator = ObjectAnimator.ofFloat(mSunView, "y", sunYStart, sunYEnd)
                 .setDuration(3000);
@@ -118,28 +113,19 @@ public class SunsetFragment extends Fragment {
                 .setDuration(3000);
         sunriseSkyAnimator.setEvaluator(new ArgbEvaluator());
 
-        ObjectAnimator sunriseSeaAnimator = ObjectAnimator
-                .ofInt(mSeaView, "backgroundColor", mSunsetSeaColor, mBlueSeaColor)
-                .setDuration(3000);
-        sunriseSeaAnimator.setEvaluator(new ArgbEvaluator());
-
         ObjectAnimator nightSkyAnimator = ObjectAnimator
-                .ofInt(mSkyView, "backgroundColor", mNightSkyColor, mSunsetSkyColor)
+                .ofInt(mSkyView,
+                        "backgroundColor",
+                        ((ColorDrawable)mSkyView.getBackground()).getColor(),
+                        mSunsetSkyColor)
                 .setDuration(1500);
         nightSkyAnimator.setEvaluator(new ArgbEvaluator());
 
-        ObjectAnimator nightSeaAnimator = ObjectAnimator
-                .ofInt(mSeaView, "backgroundColor", mNightSeaColor, mSunsetSeaColor)
-                .setDuration(1500);
-        nightSeaAnimator.setEvaluator(new ArgbEvaluator());
-
-        AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.play(heightAnimator)
+        mSunriseAnimatorSet = new AnimatorSet();
+        mSunriseAnimatorSet.play(heightAnimator)
                 .with(sunriseSkyAnimator)
                 .after(nightSkyAnimator);
-        animatorSet.play(heightAnimator)
-                .with(sunriseSeaAnimator)
-                .after(nightSeaAnimator);
-        animatorSet.start();
+        mSunsetAnimatorSet.cancel();
+        mSunriseAnimatorSet.start();
     }
 }
